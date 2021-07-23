bea349df1f47fee70356a5274c4793b88491cc06
keepsimpler <fengwenfeng@gmail.com>
2021-07-23 09:17:19 +0800

    Initialize

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+    <title>Welcome to Spacebay</title>
+	<link rel="stylesheet" type="text/css" href="/static/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="/static/css/style.css">
+</svelte:head>
+
+<script>
+    import Header from '$lib/Header.svelte';
+    import Footer from '$lib/Footer.svelte';
+</script>
+
+<div class="container-fluid px-0 pt-3 d-flex flex-column align-items-center">
+    <Header></Header>
+
+    <slot></slot>
+    
+    <Footer></Footer>
+</div>
